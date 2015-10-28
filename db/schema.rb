beb99396daf2b31c010255022ade8fcf42c54d88
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-28 14:06:36 -0400

    added comment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026150244) do
+ActiveRecord::Schema.define(version: 20151028173221) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
