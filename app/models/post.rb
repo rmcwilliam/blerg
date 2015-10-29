58281f16f6707f296c683f1badcc221b04c8d54f
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-29 11:51:39 -0400

    added association and virtual attribute

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ActiveRecord::Base
+  has_many :comments
   has_many :post_tags
   has_many :tags, through: :post_tags
+
+
+  def tag_names
+    self.tags.map(&:name).join(", ")
+  end
+
+  def tag_names=(new_tags)
+    tag_models = new_tags.split(", ").map { |name| Tag.find_or_create_by(name: name) }
+    self.tags = tag_models
+  end
 end
