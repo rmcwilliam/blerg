a3aef84ed3e8df8ea7e12d6d2e8f59f19f082896
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-10-29 21:29:15 -0400

    finished reg/sess controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,18 @@
 class RegistrationsController < ApplicationController
 
+  def new
+    @user = User.new
+    render :new # views/registar
+  end
 
-
-
-
-  
+  def create
+    @user = User.new(full_name: params[:name] 
+                     email: params[:email],
+                     password: params[:password])
+    if @user.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,21 @@
 class SessionsController < ApplicationController
 
+  def new
+    render :new # views/sessions/new.html.erb
+  end
 
+  def create 
+    user = User.find_by!(email: params[:eamil])
+    if user.password = params[:password]
+      session[:user_id] = user.user_id
+      redirect_to posts_path
+    else
+      redirect_to login_path
+    end
+  end
 
-
+  def destroy 
+    session[:user_id] = nil
+    redirect_to posts_path
+  end
 end
